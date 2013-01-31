4b9066be5113ba5fdc83956fa56c98a8b7103181
NARutkowski <nar23@njit.edu>
2013-01-31 16:52:53 +0000

    Chapter 4 using Switch Statement

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-var a = 123;
-var b = "123";
+var grade = "Premium";
 
-//equality check with strict equality operator
-if (a===b) {
-  alert("Yes, they ARE equal");
-} else {
-  alert("No, they're NOT equal")
+if ( grade === "Regular") {
+     alert("It's $3.15");
+}
+if ( grade === "Premium") {
+     alert("It's $3.35");
+}
+if ( grade === "Diesel") {
+     alert("It's $3.47");
+}
+
+//instead of using the above if statements, we can use a switch statement
+
+var grade = "Premium";
+
+switch (grade) {
+	case "Regular":
+		alert("It's $3.15");
+		break; //will jump us out of the switch statement
+	case "Premium":
+		alert("It's $3.35");
+		break;
+	case "Diesel":
+		alert("It's $3.47");
+		break;
+	default:
+		alert("That's not a valid grade");
 }
 
