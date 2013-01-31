d91fc9c8d5986790ce928a187c03ebea5db15236
NARutkowski <nar23@njit.edu>
2013-01-31 16:20:51 +0000

    Chapter 4 conditional code

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-var name = prompt("What is your name?");
-alert("Hello, " + name);
+var balance = 5000;
+
+if (balance >= 0) {
+  alert("The balance is positive");
+  if (balance > 10000) {
+    alert("The balance is large");
+  }
+}else {
+  alert("The balance is negative");
+}
+
