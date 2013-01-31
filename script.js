21826c3905332591ff8956dcf0110e537f266def
NARutkowski <nar23@njit.edu>
2013-01-31 19:21:42 +0000

    Chapter 6 while loop

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-function addTwoNumbers(a,b) { //we are declaring variables a & b
+var amount = 0;
 
-	var result = a + b;
-	alert(result);
-} 
-
-addTwoNumbers(5,10);// 5 & 10 are our arguments
-addTwoNumbers(6,20);
-addTwoNumbers(200,100);
-
-//instead of using alert we can use return, which will store the value of a + b in a variable
-function addTwoNumbers(a,b) { //we are declaring variables a & b
-
-        var result = a + b;
-        return result;
+//create the index
+var i = 0;
+//check the condition
+while (i < 10 ){
+	amount = amount +100;
+	i++; //increment index
 }
 
-var x = addTwoNumbers(5,10);// 5 & 10 are our arguments, result of function will be stored in var x
-var y = addTwoNumbers(6,20);
-var z = addTwoNumbers(200,100);
-
+alert("The value is: " + amount);
