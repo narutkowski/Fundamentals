354af68132336d5d6fb0b6efdf39dc244415d277
NARutkowski <nar23@njit.edu>
2013-01-31 23:50:20 +0000

    Chapter 10 DOM: changing headline element

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-var myArray = [500,500,500,500,500];
-
-var total = 0;
-
-for ( var i = 0 ; i < myArray.length ; i++ ) {
-    // add the current element to the total	
-    total = total + myArray[i];
-}
-
-// after we're done with the loop
-alert("The total is: " + total);
-
+var headline = document.getElementById("mainHeading");
+//changes content of healine element
+headline.innerHTML = "Wow, a new headline!";
